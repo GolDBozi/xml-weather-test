e44d278557d814ce980b1db43e8a9c348c7838cd
GolDBozi <xxxdjorkaeffxxx@gmail.com>
2017-05-07 16:06:29 +0300

    Add files via upload

---
+++ weather/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace howto_weather_forecast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnForecast = new System.Windows.Forms.Button();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtXml = new System.Windows.Forms.TextBox();
+            this.btnConditions = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnForecast
+            // 
+            this.btnForecast.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnForecast.Location = new System.Drawing.Point(195, 38);
+            this.btnForecast.Name = "btnForecast";
+            this.btnForecast.Size = new System.Drawing.Size(75, 23);
+            this.btnForecast.TabIndex = 2;
+            this.btnForecast.Text = "Forecast";
+            this.btnForecast.UseVisualStyleBackColor = true;
+            this.btnForecast.Visible = false;
+            this.btnForecast.Click += new System.EventHandler(this.btnForecast_Click);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLocation.Location = new System.Drawing.Point(69, 12);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(303, 20);
+            this.txtLocation.TabIndex = 0;
+            this.txtLocation.Text = "London";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "XML:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Location:";
+            // 
+            // txtXml
+            // 
+            this.txtXml.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtXml.Location = new System.Drawing.Point(15, 95);
+            this.txtXml.Multiline = true;
+            this.txtXml.Name = "txtXml";
+            this.txtXml.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtXml.Size = new System.Drawing.Size(48, 154);
+            this.txtXml.TabIndex = 3;
+            this.txtXml.WordWrap = false;
+            // 
+            // btnConditions
+            // 
+            this.btnConditions.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnConditions.Location = new System.Drawing.Point(114, 38);
+            this.btnConditions.Name = "btnConditions";
+            this.btnConditions.Size = new System.Drawing.Size(75, 23);
+            this.btnConditions.TabIndex = 1;
+            this.btnConditions.Text = "Conditions";
+            this.btnConditions.UseVisualStyleBackColor = true;
+            this.btnConditions.Click += new System.EventHandler(this.btnConditions_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnConditions);
+            this.Controls.Add(this.txtXml);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnForecast);
+            this.Name = "Form1";
+            this.Text = "howto_weather_forecast";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnForecast;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtXml;
+        private System.Windows.Forms.Button btnConditions;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ weather/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace howto_weather_forecast
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Enter your API key here.
+        // Get an API key by making a free account at:
+        //      http://home.openweathermap.org/users/sign_in
+        private const string API_KEY = "e1053d1b85007fa1f764226b8401b627";
+
+        // Query URLs. Replace @LOC@ with the location.
+        private const string CurrentUrl =
+            "http://api.openweathermap.org/data/2.5/weather?" +
+            "q=@LOC@&mode=xml&units=metric&APPID=" + API_KEY;
+        private const string ForecastUrl =
+            "http://api.openweathermap.org/data/2.5/forecast?" +
+            "q=@LOC@&mode=xml&units=imperial&APPID=" + API_KEY;
+
+        // Get current conditions.
+        private void btnConditions_Click(object sender, EventArgs e)
+        {
+            // Compose the query URL.
+            string url = CurrentUrl.Replace("@LOC@", txtLocation.Text);
+            txtXml.Text = GetFormattedXml(url);
+        }
+
+        // Get a forecast.
+        private void btnForecast_Click(object sender, EventArgs e)
+        {
+            // Compose the query URL.
+            string url = ForecastUrl.Replace("@LOC@", txtLocation.Text);
+            txtXml.Text = GetFormattedXml(url);
+        }
+
+        // Return the XML result of the URL.
+        private string GetFormattedXml(string url)
+        {
+            // Create a web client.
+            using (WebClient client = new WebClient())
+            {
+                // Get the response string from the URL.
+                string xml = client.DownloadString(url);
+
+                // Load the response into an XML document.
+                XmlDocument xml_document = new XmlDocument();
+                xml_document.LoadXml(xml);
+              //  XmlNodeList xnList = xml_document.SelectNodes("/Names/Name[@type='M']");
+                // Format the XML.
+                using (StringWriter string_writer = new StringWriter())
+                {
+                    XmlTextWriter xml_text_writer = new XmlTextWriter(string_writer);
+                    xml_text_writer.Formatting = Formatting.Indented;
+                    xml_document.WriteTo(xml_text_writer);
+
+                    // Return the result.
+                    return string_writer.ToString();
+                }
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = CurrentUrl.Replace("@LOC@", txtLocation.Text);
+
+            using (StreamWriter sw = new StreamWriter(@"weather.txt"))
+            {
+               sw.WriteLine(GetFormattedXml(url));
+            }
+            //XmlSerializer serial = new XmlSerializer(typeof(string));
+            //using (FileStream fs = new FileStream(Environment.CurrentDirectory + "\\weather.xml", FileMode.Create, FileAccess.Write))
+            //{ serial.Serialize(fs, GetFormattedXml(url)); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(@"weather.txt");
+            label3.Text = "Temperature :" + lines[9].Remove(0, 22).Remove(3);
+            label4.Text = "Weather Type :"+ lines[29].Remove(0,31).Remove(15);
+
+            //    XmlDataDocument xmldoc = new XmlDataDocument();
+            //    XmlNodeList xmlnode;
+            //    XmlNodeList xmlnode1;
+            //   string keimeno = null;
+            //    string keimeno1 = null;
+
+
+            //    FileStream fs = new FileStream("weather.xml", FileMode.Open, FileAccess.Read);
+
+            //    xmldoc.Load(fs);
+            //    xmlnode = xmldoc.GetElementsByTagName("temperature value");
+            //    xmlnode1 = xmldoc.GetElementsByTagName("weather");
+            //    keimeno = xmlnode[i].ChildNodes.Item(0).InnerText.Trim();
+            //    keimeno1= xmlnode1[i].ChildNodes.Item(0).InnerText.Trim();
+
+            //    label1.Text = keimeno;
+            //    label2.Text = keimeno1;
+
+
+            //    fs.Close();
+        }
+    }
+}
